0c9922817998ef39ad96bf4da83c49fd0e6e80d0
chongshan <chongsh1@ualberta.ca>
2026-03-05 16:32:46 -0700

    commit add hasCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        // hasCity() 还不存在时，这里会报错/失败（这就是 RED）
+        assertTrue(list.hasCity(calgary));
+    }
+
 }
